2d1df505c744979c0b25d8ee932152d0f5c49ee5
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-11-10 10:26:49 +0700

    Change route int to str

--- railway/urls.py
+++ railway/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('', views.main_page, name='main_page'),
     path('routes/search/', views.search_route, name='search_route'),
-    path('seats/pick/<int:route_id>/', views.pick_seat, name='pick_seat'),
+    path('seats/pick/<route_id>/', views.pick_seat, name='pick_seat'),
     path('tickets/', views.ticket_information, name='ticket_information'),
-    path('tickets/<int:ticket_id>/', views.ticket_detail, name='ticket_detail'),
-    path('reservation/create/<int:route_id>/', views.create_reservation, name='create_reservation'),
+    path('tickets/<ticket_id>/', views.ticket_detail, name='ticket_detail'),
+    path('reservation/create/<route_id>/', views.create_reservation, name='create_reservation'),
 ]
