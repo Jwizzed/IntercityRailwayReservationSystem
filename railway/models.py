92048ebc3a43198375ea256e826e83186322540a
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-11-10 11:01:37 +0700

    added Seat

--- railway/models.py
+++ railway/models.py
@@ -1,17 +1,21 @@
 from django.core.validators import MinValueValidator
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import EmailValidator
 from django.utils import timezone
 
 from django.db import models
 
 
 class Passenger(AbstractUser):
-    phone_number = models.CharField(max_length=15, blank=True)
-    passport_number = models.CharField(max_length=9, blank=True)
-    age = models.IntegerField(blank=True, null=True)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    ciz_id = models.CharField(max_length=9, blank=True)
+    email = models.EmailField(validators=[EmailValidator])
+    phone = models.CharField(max_length=15, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return f"Passenger {self.username}"
+        return f"{self.firstname} {self.lastname}"
 
 
 class Station(models.Model):
@@ -27,11 +31,18 @@ class Station(models.Model):
 
 class Train(models.Model):
     train_id = models.CharField(primary_key=True, max_length=255)
-    train_type = models.CharField(max_length=255)
+    train_type_str = models.CharField(max_length=255)
     manufacture_year = models.IntegerField(validators=[MinValueValidator(1900)])
 
     def __str__(self):
-        return f"{self.train_type} - {self.train_id}"
+        return f"{self.train_type_str} - {self.train_id}"
+
+class Seat(models.Model):
+    seat_id = models.CharField(primary_key=True, max_length=255)
+    train = models.ForeignKey(Train, on_delete=models.CASCADE, related_name='seats')
+
+    def __str__(self):
+        return f"Seat {self.seat_id} on train {self.train.train_id}"
 
 
 class Route(models.Model):
@@ -51,6 +62,7 @@ class Ticket(models.Model):
     ticket_id = models.CharField(primary_key=True, max_length=255)
     route = models.ForeignKey(Route, on_delete=models.CASCADE)
     price = models.IntegerField(validators=[MinValueValidator(0)])
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.ticket_id}"
@@ -65,4 +77,4 @@ class Reservation(models.Model):
     to_station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='reservations_to_station')
 
     def __str__(self):
-        return f"Reservation {self.rev_id} by {self.passenger.name}"
+        return f"Reservation {self.rev_id} by {self.passenger.firstname}"
