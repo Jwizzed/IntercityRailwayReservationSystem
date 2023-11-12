19a72ea94cdc56479d191a59323aa7cd38a52609
MeHappyLucky <menggy2546@gmail.com>
2023-11-12 18:14:11 +0700

    add wow themed data

--- railway/admin.py
+++ railway/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Station, Train, Route, Ticket, Reservation, Passenger
+from .models import Station, Train, Route, Ticket, Reservation, Passenger, Seat
 
 admin.site.register(Passenger)
 admin.site.register(Station)
@@ -8,3 +8,4 @@ admin.site.register(Train)
 admin.site.register(Route)
 admin.site.register(Ticket)
 admin.site.register(Reservation)
+admin.site.register(Seat)
